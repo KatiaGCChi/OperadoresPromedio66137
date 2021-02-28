ac494933d1d10f81190c592bb48b76aa21367296
TRUJEQUE <TRUJEQUE@ANDREA>
2021-02-28 15:16:03 -0600

    operadorespromedio

---
+++ src/operadorespromedio66137/OperadoresPromedio66137.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package operadorespromedio66137;
+
+/**
+ *
+ * @author TRUJEQUE
+ */
+public class OperadoresPromedio66137 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int num1 , num2 , num3 , num4 , num5 ;
+        num1 = 15 ;
+        num2 = 20 ;
+        num3 = 25 ;
+        num4 = 30 ;
+        num5 = 35 ;
+        double promedio = ( (num1 + num2 + num3 + num4 + num5) / 5) ;
+        System.out.print("El promedio es: " + promedio + "\n") ;
+        
+        
+    }
+    
+}
